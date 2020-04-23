1c5a5a72ca6e8ef53aebefe59f752c4ce402082f
Lucas Bost <lucas.bost@epfl.ch>
2020-04-23 09:13:50 +0200

    added files to project

---
+++ projet_microinfo/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <motors.h>
+#include <camera/po8030.h>
+#include <chprintf.h>
+#include <VL53L0X.h>
+#include <messagebus.h>
+#include <i2c_bus.h>
+
+#include <pi_regulator.h>
+#include <process_image.h>
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+void SendUint8ToComputer(uint8_t* data, uint16_t size) 
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+}
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    //starts the serial communication
+    serial_start();
+    i2c_start();
+    //start the USB communication
+    usb_start();
+    /*//starts the camera
+    dcmi_start();
+	po8030_start();*/
+	//inits the motors
+	motors_init();
+
+	/*//stars the threads for the pi regulator and the processing of the image
+	pi_regulator_start();
+	process_image_start();*/
+
+    VL53L0X_start();
+
+
+
+    /* Infinite loop. */
+    while (1) {
+    	uint16_t speed;
+    	uint16_t mesure = 0;
+    	mesure = VL53L0X_get_dist_mm();
+    	chprintf((BaseSequentialStream *)&SD3, "Distance = %d\n", mesure);
+    	//waits 1 second
+    	if(mesure < D_MAX){
+    		speed = STOP;
+    		right_motor_set_speed(speed);
+			left_motor_set_speed(speed);
+    	}
+    	else {
+    		speed = START;
+    		right_motor_set_speed(speed);
+    		left_motor_set_speed(-speed);
+    	}
+
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
