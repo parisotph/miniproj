26a52ba3fac487db7daf7b0b4c22e0d78eb491c6
Lucas Bost <lucas.bost@epfl.ch>
2020-05-09 11:15:31 +0200

    filter.h

---
+++ projet_microinfo/filter.h
@@ -0,0 +1,21 @@
+/*
+ * filter.h
+ *
+ *  Created on: 7 mai 2020
+ *      Author: Philippe Parisot and Lucas Bost
+ */
+
+#ifndef FILTER_H_
+#define FILTER_H_
+
+void filter_start(void);
+
+uint8_t get_cnt1(void);
+
+uint8_t get_cnt2(void);
+
+uint8_t get_cnt3(void);
+
+uint8_t get_cnt4(void);
+
+#endif /* FILTER_H_ */
